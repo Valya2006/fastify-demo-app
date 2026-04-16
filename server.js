104677bb6a5b4b25f1e2056929a6ad821da586ef
valya <valia2006@DESKTOP-KJRGD7H.localdomain>
2026-04-16 13:29:12 +0300

    the end

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import Fastify from 'fastify';
 import fastifyStatic from '@fastify/static';
+import view from '@fastify/view';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import pug from 'pug';
+import formBody from '@fastify/formbody';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,10 +16,43 @@ app.register(fastifyStatic, {
     root: path.join(__dirname, 'public')
 });
 
-app.get('/api', (req, res) => {
-	res.send('Запрос прошел успешно')
+app.register(view, {
+  engine: {
+    pug: pug,
+  },
+  root: path.join(__dirname, 'views')
+});
+
+app.register(formBody);
+
+const users = [
+  { id: 1, name: 'Алексей Смирнов', email: 'alexey@example.com' },
+  { id: 2, name: 'Мария Иванова', email: 'maria@example.com' },
+  { id: 3, name: 'Дмитрий Петров', email: 'dmitry@example.com' },
+  { id: 4, name: 'Елена Сидорова', email: 'elena@example.com' },
+  { id: 5, name: 'Константин Воробьев', email: 'kostya@example.com' }
+];
+app.get('/', (req, res) => {
+	res.redirect('/users')
 })
 
+app.get('/users', (req, res) => {
+	res.view('users', { users })
+})
+
+app.get('/users/create', (req, res) => {
+	res.view('createUsers')
+})
+
+app.post('/users', (req, res) => {
+	const { name, email } = req.body
+	const id = users.length + 1
+	const user = { id, name, email}
+	users.push(user)
+
+	res.redirect('/users')
+
+})
 app.listen({ port }, (err) => {
     console.log('Сервер запущен на http://localhost:3000');
 });
